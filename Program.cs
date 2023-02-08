d0d66c407e9524d22c997702bde15455ced8564e
Lucas Celestino <lucascap899@gmail.com>
2023-02-08 19:54:15 -0300

    adicionando dbcontext como serviço

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
